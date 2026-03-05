7b1e88c6aba272dda9b96dd80bb6fa2690a17ffa
Glib Cherniev <glibcherniev@gmail.com>
2026-03-05 11:05:24 +0200

    laba6

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home, category_view
+from .views import home, category_view, product_detail, categories_list
 
 urlpatterns = [
     path('', home, name='home'),
+    path('categories/', categories_list, name='categories'),
     path('category/<int:id>/', category_view, name='category'),
+    path('product/<int:id>/', product_detail, name='product'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 def home(request):
@@ -14,5 +14,20 @@ def category_view(request, id):
     categories = Category.objects.all()
     return render(request, "shop/home.html", {
         "products": products,
+        "categories": categories
+    })
+
+
+def product_detail(request, id):
+    product = get_object_or_404(Product.objects.prefetch_related('images'), id=id)
+    categories = Category.objects.all()
+
+    return render(request, "shop/product_detail.html", {
+        "product": product,
+        "categories": categories
+    })
+def categories_list(request):
+    categories = Category.objects.all()
+    return render(request, "shop/categories.html", {
         "categories": categories
     })
